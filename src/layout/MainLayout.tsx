fe55716da52250e0cb592dd7f0cecaa1584bd3cc
riasat01 <riasat01@outlook.com>
2024-12-12 13:09:43 +0600

    feat: add navbar and footer

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,17 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 
 const MainLayout = () => {
     return (
-        <nav>
-            <li>home</li>
-            <li>about</li>
-        </nav>
+        <section>
+            <Navbar />
+            <div className="min-h-screen">
+                <Outlet />
+            </div>
+            <Footer />
+        </section>
     );
 };
 
